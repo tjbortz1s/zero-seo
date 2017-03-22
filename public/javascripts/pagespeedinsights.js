e76982344de6f07b8ef0bf0849fb8e0175f66cd7
tjbortz1s <tjbortz1s@gmail.com>
2017-03-22 17:45:41 -0500

    Wed Meeting Work
    This is what we added during wednesday's meeting.  Mostly just work on
    the angular page and correction to how the JSON api return is returned
    by the server's api call.

--- public/javascripts/pagespeedinsights.js
+++ public/javascripts/pagespeedinsights.js
@@ -3,15 +3,15 @@ var request= require('request');
 var pageInsightsQuery = function(url, callback){
   var apikey = "AIzaSyCNaOdODmRoJTDbt6xf_XEdTxcA44rBB10";
 	var getURL = function(url) {
-		return 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url='+url+'&key='+apikey;
+    var theURL = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url='+url+'&key='+apikey;
+    console.log("URL: " + theURL);
+		return theURL;
 	}
 
 	if(url != "") {
-    console.log(url);
 		var requestURL = getURL(url);
-    console.log(requestURL);
 		var results = request(requestURL, function(error, response, body) {
-      callback(body);
+      callback(JSON.parse(body));
     });
   }
 }
