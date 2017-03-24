60585d48d6f96240c11bc5368932b629f0bb8d4f
tjbortz1s <tjbortz1s@gmail.com>
2017-03-24 00:43:36 -0500

    Loading Bar, Beginning text-formatting
    Added a loadng bar.  Began to work on manipulating the API results to be
    able to format the text as is needed for the page to display properly
    without the {{}} tags.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,16 +4,42 @@ app.controller('myController', function($scope, $http) {
   $scope.text = "https://www.google.com";
   $scope.jsonfile = "";
   $scope.showEntryInfo = true;
+  $scope.showLoader = false;
 
   $scope.submit = function() {
     if($scope.text) {
       $scope.showEntryInfo = false;
+      $scope.showLoader = true;
       var request = $http.post('/api/speedTest', {'url': $scope.text, ismobile: false});
+      //need a few different variables and actions
+      //one, the arguments need to be used in the descriptions, links and so on
+      //should be turned into html? or something of that sort
+
+      //two, there should be a few variables
+      //overall score
+      //total score
+      //amount each group contributes to score
+
+      //make it so that the groupScores shows the total scores for each group.
+      /*$scope.groupScores = data.ruleGroups;
+      $scope.pageStats = data.pageStats;
+      //{{beginlink}} and {{endlink}} should be replaced with <href> tags as is appropriate
+      var formattedArray = [];
+      data.formattedResults.ruleResults.forEach(function(ruleData){
+        var tempObject = [];
+
+      });*/
+
+      console.log("GOT HERE GUYA");
+      //this should all still work
+
       request.success(function(data){
         $scope.jsonfile = data;
+        $scope.showLoader = false;
       });
       request.error(function(data) {
         console.log('Error ' + data);
+        $scope.showLoader = false;
       });
       $scope.text = "";
     }
---
+++ public/javascripts/headerArgsManager.js
@@ -0,0 +1,9 @@
+//https://www.npmjs.com/package/string
+
+
+var headerArgsManager = function(header, args){
+  //header should be a string
+  //args will be an array/object
+}
+
+module.exports = headerArgsManager;
